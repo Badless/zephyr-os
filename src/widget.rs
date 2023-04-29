e301c82defb412b7627674d9a5758682523f1e64
DevBrainless <79766539+Badless@users.noreply.github.com>
2023-04-29 16:26:21 +0200

    Update widget.rs

--- src/widget.rs
+++ src/widget.rs
@@ -10,6 +10,18 @@ pub fn clock(ctx: &egui::Context, open: &mut bool) {
         });
 }
 
+pub fn settings(ctx: &egui::Context, open: &mut bool) {
+    egui::Window::new("Settings")
+        .anchor(egui::Align2::LEFT_BOTTOM, egui::Vec2::ZERO)
+        .movable(false)
+        .resizable(false)
+        .title_bar(false)
+        .open(open)
+        .show(ctx, |ui| {
+            ctx.style_ui(ui);
+        });
+}
+
 /*
     If you can help We really want widget system to work like:
 
